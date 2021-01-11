d78360580436bc82a2f6a4bc1814e7f488221dd9
Issa Rice <riceissa@gmail.com>
2021-01-11 11:57:42 -0800

    Use interval_anchor when graphing

--- review_load.py
+++ review_load.py
@@ -5,16 +5,17 @@
 import matplotlib.pyplot as plt
 import datetime
 import sqlite3
+import sys
 
 
-conn = sqlite3.connect('data.db')
+conn = sqlite3.connect(sys.argv[1])
 cur = conn.cursor()
-data = cur.execute("select last_reviewed_on, interval from notes where interval >= 0")
+data = cur.execute("select interval_anchor, interval from notes where interval >= 0")
 
 due_ins = []
-for last_reviewed_on_, interval in data:
-    last_reviewed_on = datetime.datetime.strptime(last_reviewed_on_, "%Y-%m-%d")
-    due_on = last_reviewed_on + datetime.timedelta(days=interval)
+for interval_anchor_, interval in data:
+    interval_anchor = datetime.datetime.strptime(interval_anchor_, "%Y-%m-%d")
+    due_on = interval_anchor + datetime.timedelta(days=interval)
     due_in = (due_on - datetime.datetime.today()).days
     due_ins.append(due_in)
 
