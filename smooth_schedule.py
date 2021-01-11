7c4c78f7ec680465ada0fb361af4702444320864
Issa Rice <riceissa@gmail.com>
2021-01-11 13:06:44 -0800

    typo

--- smooth_schedule.py
+++ smooth_schedule.py
@@ -16,7 +16,7 @@ notes = [note for note in script.get_notes_from_db(conn) if note.interval >= 0]
 
 def get_due_date(note):
     # We use last_reviewed_on here instead of interval_anchor; it's the
-    # "natural" review date. If I repeatedly smooth the schedule, then some
+    # "natural" review date. If we repeatedly smooth the schedule, then some
     # notes might get prioritized in weird ways we were to use interval_anchor.
     # Using last_reviewed_on should be more stable under multiple runs of the
     # smoother algorithm.
