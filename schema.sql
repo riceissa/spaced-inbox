d4cea351094d9597a2f43ce6bf7933c9e000d8e8
Issa Rice <riceissa@gmail.com>
2021-01-10 21:49:40 -0800

    Update notes table schema
    I doubt there are any existing users besides me, but if there are, here
    is what you need to do:
    
    From the shell, type `sqlite3 data.db` to enter the sqlite prompt, then
    type:
        alter table notes add column interval_anchor date;
        update notes set interval_anchor = last_reviewed_on;
    You can then exit sqlite.

--- schema.sql
+++ schema.sql
@@ -8,5 +8,13 @@ create table notes (
         line_number_end integer,
         ease_factor integer,  /* as a percentage, defaults to 250% like in Anki */
         interval integer,  /* in days; -1 means the note has been soft-deleted */
-        last_reviewed_on date
+        last_reviewed_on date,
+        /* Usually the same as last_reviewed_on, but this allows us to
+           keep track of the last date on which a card was reviewed
+           separately from the scheduling calculation (which is calcualted
+           as interval_anchor + interval). This is useful for smoothing
+           out the review schedule (if we didn't know the
+           last_reviewed_on date, then we might accidentally keep pushing
+           certain cards out in the future without ever reviewing them). */
+        interval_anchor date
 );
