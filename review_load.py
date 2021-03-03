cf57a53b9e63fa038ec35efc7ebcad33ee2b77d3
Issa Rice <riceissa@gmail.com>
2021-03-03 10:56:15 -0800

    Handle multiple inbox files

--- review_load.py
+++ review_load.py
@@ -10,32 +10,35 @@ import sys
 
 conn = sqlite3.connect(sys.argv[1])
 cur = conn.cursor()
-data = cur.execute("select interval_anchor, interval from notes where interval >= 0")
+data = cur.execute("select interval_anchor, interval, filepath from notes where interval >= 0")
 
-due_ins = []
-for interval_anchor_, interval in data:
+filepaths = set()
+
+due_ins = {}
+for interval_anchor_, interval, filepath in data:
     interval_anchor = datetime.datetime.strptime(interval_anchor_, "%Y-%m-%d")
     due_on = interval_anchor + datetime.timedelta(days=interval)
     due_in = (due_on - datetime.datetime.today()).days
-    due_ins.append(due_in)
+    due_ins[filepath] = due_ins.get(filepath, []) + [due_in]
+    filepaths.add(filepath)
 
-fig, axs = plt.subplots(2)
+fig, axs = plt.subplots(1)
 
-axs[0].hist(due_ins, bins=200)
-
-due_dict = {}
-for x in due_ins:
-    due_dict[x] = due_dict.get(x, 0) + 1
-xs = list(range(min(due_ins), max(due_ins) + 1))
-ys = []
-for x in xs:
-    if x in due_dict:
-        ys.append(due_dict[x])
-    else:
-        ys.append(0)
-axs[1].plot(xs, ys)
+for fp in filepaths:
+    due_dict = {}
+    for x in due_ins[fp]:
+        due_dict[x] = due_dict.get(x, 0) + 1
+    xs = list(range(min(due_ins[fp]), max(due_ins[fp]) + 1))
+    ys = []
+    for x in xs:
+        if x in due_dict:
+            ys.append(due_dict[x])
+        else:
+            ys.append(0)
+    axs.plot(xs, ys, label=fp)
 
 
 plt.xlabel("days in the future")
 plt.ylabel("number of notes due for review on this day")
+plt.legend(loc="upper right")
 plt.show()
