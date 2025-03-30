5d16f11ba2b0e71ca59683ed63107cacf1cd521e
Issa Rice <riceissa@gmail.com>
2025-03-30 14:50:31 -0700

    new schema

---
+++ migrate_20250330.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# Script to import an old database (pre-March 2025) to the new database
+# schema.
+
+import sys
+import sqlite3
+
+sys.stdout.reconfigure(encoding='utf-8')  # type: ignore
+
+conn_old = sqlite3.connect('data-2025-03-30.db.bak')
+c_old = conn_old.cursor()
+
+conn_new = sqlite3.connect('data_new.db')
+c_new = conn_new.cursor()
+with open("schema.sql", "r", encoding="utf-8") as f:
+    c_new.executescript(f.read())
+
+fetched = c_old.execute("""
+        select sha1sum, note_text, line_number_start, line_number_end, ease_factor, interval, last_reviewed_on, interval_anchor, inbox_name, created_on, reviewed_count, note_state from notes
+        """).fetchall()
+
+for row in fetched:
+    old_sha1sum = row[0]
+    old_note_text = row[1]
+    old_line_number_start = row[2]
+    old_line_number_end = row[3]
+    old_ease_factor = row[4]
+    old_interval = row[5]
+    old_last_reviewed_on = row[6]
+    old_interval_anchor = row[7]  # not used
+    old_inbox_name = row[8]  # not used
+    old_created_on = row[9]
+    old_reviewed_count = row[10]
+    old_note_state = row[11]
+
+    new_note_state = "normal" if old_note_state == "just created" else old_note_state
+    c_new.execute("""
+        insert into notes (sha1sum, line_number_start, line_number_end, ease_factor, interval, last_reviewed_on, created_on, reviewed_count, note_state, note_text) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, (old_sha1sum, old_line_number_start, old_line_number_end, old_ease_factor, old_interval, old_last_reviewed_on, old_created_on, old_reviewed_count, new_note_state, old_note_text))
+
+    conn_new.commit()
--- schema.sql
+++ schema.sql
@@ -3,27 +3,11 @@ drop table if exists notes;
 create table notes (
         id integer primary key autoincrement,
         sha1sum text unique not null,
-        note_text text,
         line_number_start integer,
         line_number_end integer,
         ease_factor integer,  /* as a percentage, defaults to 250% like in Anki */
         interval integer,  /* in days; -1 means the note has been soft-deleted */
         last_reviewed_on date,
-        /* Usually the same as last_reviewed_on, but this allows us to
-           keep track of the last date on which a card was reviewed
-           separately from the scheduling calculation (which is calcualted
-           as interval_anchor + interval). This is useful for smoothing
-           out the review schedule (if we didn't know the
-           last_reviewed_on date, then we might accidentally keep pushing
-           certain cards out in the future without ever reviewing them).
-
-           2025-03-30: I am pretty sure this field is not needed anymore
-           because I improved the scheduling algorithm and stopped showing
-           users "due notes" (which are a stressful concept), smoothing is no
-           longer necessary to perform.
-         */
-        interval_anchor date,
-
         created_on date,
 
         /* Number of times note has been reviewed; if a note is modified it is
@@ -43,15 +27,15 @@ create table notes (
            - "yeah" (agreement)
            - "lol" (funny/amusing)
 
-           The note state (except for "just created") also corresponds to the
+           The note state (except for "normal") also corresponds to the
            response that was given to the note when the note was
            last reviewed.
 
            The responses are intended to mimic the kinds of responses one would
            give upon reading a chat message from a friend... you would respond
            saying "yeah" or "that's cool" or whatever.  If you have a more
-           detailed response, of course you can just edit the note! It's your
-           writing inbox.  This "response as chat message" idea seems like
+           detailed response, of course you can just edit the note! -- it's your
+           writing inbox, after all.  This "response as chat message" idea seems like
            a better way to conceive of inbox responses than e.g. Anki's
            Easy/Good/Hard/Again.  But I'm still actively thinking of
            what the correct "primitives" are or "how to conceptualize
@@ -59,5 +43,7 @@ create table notes (
            - https://wiki.issarice.com/wiki/Interaction_reversal_between_knowledge-to-be-memorized_and_ideas-to-be-developed
            - https://wiki.issarice.com/wiki/Mapping_mental_motions_to_parts_of_a_spaced_repetition_algorithm
          */
-        note_state text
+        note_state text,
+
+        note_text text
 );
