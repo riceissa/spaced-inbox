234bfe4366680cb674dc3572500c5b42464b9c6e
Issa Rice <riceissa@gmail.com>
2025-04-12 13:09:34 -0700

    bake in data.db path and fix variable names to make it less confusing

--- plot_histogram_review_load.py
+++ plot_histogram_review_load.py
@@ -2,40 +2,41 @@
 
 # This script displays a histogram of your future review load.
 
+from pathlib import Path
 import matplotlib.pyplot as plt
 import datetime
 import sqlite3
 import sys
 
 
-conn = sqlite3.connect(sys.argv[1])
+conn = sqlite3.connect(Path("~/.local/share/spaced-inbox/data.db").expanduser())
 cur = conn.cursor()
 data = cur.execute("select last_reviewed_on, interval, filepath from notes where interval >= 0")
 
-names = set()
+filepaths = set()
 
 due_ins = {}
-for interval_anchor_, interval, name in data:
-    interval_anchor = datetime.datetime.strptime(interval_anchor_, "%Y-%m-%d")
-    due_on = interval_anchor + datetime.timedelta(days=interval)
+for last_reviewed_on_str, interval, filepath in data:
+    last_reviewed_on = datetime.datetime.strptime(last_reviewed_on_str, "%Y-%m-%d")
+    due_on = last_reviewed_on + datetime.timedelta(days=interval)
     due_in = (due_on - datetime.datetime.today()).days
-    due_ins[name] = due_ins.get(name, []) + [due_in]
-    names.add(name)
+    due_ins[filepath] = due_ins.get(filepath, []) + [due_in]
+    filepaths.add(filepath)
 
 fig, axs = plt.subplots(1)
 
-for name in names:
+for filepath in filepaths:
     due_dict = {}
-    for x in due_ins[name]:
+    for x in due_ins[filepath]:
         due_dict[x] = due_dict.get(x, 0) + 1
-    xs = list(range(min(due_ins[name]), max(due_ins[name]) + 1))
+    xs = list(range(min(due_ins[filepath]), max(due_ins[filepath]) + 1))
     ys = []
     for x in xs:
         if x in due_dict:
             ys.append(due_dict[x])
         else:
             ys.append(0)
-    axs.plot(xs, ys, label=name)
+    axs.plot(xs, ys, label=filepath)
 
 
 plt.xlabel("days in the future")
